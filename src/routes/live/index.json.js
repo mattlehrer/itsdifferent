325bcf66e7a7942e00eafeec1f516f17a6f347e0
Matt Lehrer <lehrerm@gmail.com>
2020-03-28 19:30:42 -0700

    prettier

--- src/routes/live/index.json.js
+++ src/routes/live/index.json.js
@@ -5,8 +5,26 @@ const client = contentful.createClient({
   accessToken: 'process.env.CONTENTFUL_ACCESS_TOKEN',
 });
 
-const categories = ['sevenDays', 'weekdays', 'sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
-const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const categories = [
+  'sevenDays',
+  'weekdays',
+  'sunday',
+  'monday',
+  'tuesday',
+  'wednesday',
+  'thursday',
+  'friday',
+  'saturday',
+];
+const days = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
 
 export async function get(req, res) {
   res.writeHead(200, {
@@ -18,23 +36,29 @@ export async function get(req, res) {
     const data = await client.getEntries({ content_type: category });
     allData[category] = data.items.map(time => {
       return {
-        ...time.fields
+        ...time.fields,
       };
     });
   }
   let dataByDay = {};
   for (const day of days) {
-    dataByDay[day] = allData[day.toLowerCase()]
-    const classesToAdd = ['Saturday', 'Sunday'].includes(day) ? allData['sevenDays'] : [...allData['weekdays'], ...allData['sevenDays']];
+    dataByDay[day] = allData[day.toLowerCase()];
+    const classesToAdd = ['Saturday', 'Sunday'].includes(day)
+      ? allData['sevenDays']
+      : [...allData['weekdays'], ...allData['sevenDays']];
     for (const pair of classesToAdd) {
       const existingTimes = dataByDay[day].map(time => time.time);
       if (dataByDay[day].length && pair.time in existingTimes) {
-        dataByDay[day][existingTimes.indexOf(pair.time)].liveClasses.push(...pair.liveClasses);
+        dataByDay[day][existingTimes.indexOf(pair.time)].liveClasses.push(
+          ...pair.liveClasses,
+        );
       } else {
         dataByDay[day].push(pair);
       }
     }
-    dataByDay[day] = dataByDay[day].sort((a, b) => a.time.replace(':', '') - b.time.replace(':', ''))
+    dataByDay[day] = dataByDay[day].sort(
+      (a, b) => a.time.replace(':', '') - b.time.replace(':', ''),
+    );
   }
   // console.log(dataByDay);
   res.end(JSON.stringify(dataByDay));
